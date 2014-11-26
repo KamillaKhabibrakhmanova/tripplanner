d9365c4c2135e774534148ac4d73f00c11f2b63e
KamillaKhabibrakhmanova <venessiel@gmail.com>
2014-11-26 11:55:24 -0500

    working on setup

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
+var sass = require('node-sass');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,18 +14,29 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + npm'/public/favicon.ico'));
+app.use('bower_components', express.static(__dirname + '/bower_components'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+    sass.middleware({
+        src: __dirname + '/assets',
+        dest: __dirname + '/public',
+        debug: true
+    })
+);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +50,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+    swig.setDefaults( {cache: false});
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
