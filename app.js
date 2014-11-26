4bd18cc2fe7f8d88fc5bd14e2d7c67c817270e69
justincoh <justincoh@gmail.com>
2014-11-26 13:01:25 -0500

    All tied together

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.engine('html', swig.renderFile);
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(__dirname + npm'/public/favicon.ico'));
-app.use('bower_components', express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var q = require('q');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+  var hotels;
+  var restaurants;
+  var things;
 
+
+  ////Callback HELL
+  // models.Hotel.find(function(err, hotelResults) {
+  //   hotels = hotelResults;
+	 //    models.Restaurant.find(function(err, restaurantResults) {
+	 //  		restaurants=restaurantResults;
+		// 	  	models.ThingsToDo.find(function(err, thingResults) {
+		// 		  	things=thingResults;
+				  	
+		  		 		// res.render('index', { hotels: hotels,
+						  	// title: "Trip Planner",
+						  	// restaurants : restaurants,
+						  	// things_to_do: things })	
+		//   		})
+	 //  	})
+  // });
+
+
+	var hotelPromise = models.Hotel.find().exec();  //.exec method returns a promise
+	var resPromise = models.Restaurant.find().exec();
+	var thingsPromise = models.ThingsToDo.find().exec();
+
+	q.all([hotelPromise,resPromise,thingsPromise]).then(function(results){
+		console.log(results[0])
+		res.render('index', { 
+				title: "Trip Planner",
+				hotels: results[0],
+			  	restaurants : results[1],
+			  	things_to_do: results[2] }
+		)
+
+	})
+
+})
 module.exports = router;
+
+
+ 
