6914ef2cd2949179e8d3cd735a5199e40bb0b4bf
KamillaKhabibrakhmanova <venessiel@gmail.com>
2014-12-01 16:55:03 -0500

    Added add to say functionality

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,90 +1,127 @@
 $(document).on('ready', function() {
-	var button = "<button type=\"button\" class=\"btn btn-default\">";
-	var count = 4;
-	var hotels = $('#hotels');
+    var button = "<button type=\"button\" class=\"btn btn-default\">";
+    var count = 4;
+    var hotels = $('#hotels');
 
-	///Populating dropdowns, adding database ID to URL
-	all_hotels.forEach(function(element){
-		hotels.append('<li id=\"'+element._id+'\">'+element.name+'</a></li>')
-		})
-	var things = $('#things');
-	all_things_to_do.forEach(function(element){
-		things.append('<li id=\"'+element._id+'\">'+element.name+'</a></li>')
-		})
+    ///Populating dropdowns, adding database ID to URL
+    all_hotels.forEach(function(element) {
+        hotels.append('<li id=\"' + element._id + '\">' + element.name + '</a></li>')
+    })
+    var things = $('#things');
+    all_things_to_do.forEach(function(element) {
+        things.append('<li id=\"' + element._id + '\">' + element.name + '</a></li>')
+    })
 
-	var restaurants = $('#restaurants');
-	all_restaurants.forEach(function(element){
-		restaurants.append('<li id=\"'+element._id+'\">'+element.name+'</a></li>')
-		})
-	$('#add_day').on('click', function() {
-		$('#button-row :nth-child('+ (count-1) + ')').after(button + "Day " + count + "</button>");
-		count ++;
-	});
-
-	
+    var restaurants = $('#restaurants');
+    all_restaurants.forEach(function(element) {
+        restaurants.append('<li id=\"' + element._id + '\">' + element.name + '</a></li>')
+    })
+    $('#add_day').on('click', function() {
+        $('#button-row :nth-child(' + (count - 1) + ')').after(button + "Day " + count + "</button>");
+        count++;
+    });
 
 
-   	$(".dropdown-menu").on('click', 'li', function(){
-		$(this).parent().prev().text($(this).text());
-		console.log(this.id)
-  	});
-
-	
-	////Google Maps
-
-	var markers=[];
 
 
-	function newMarker (lat,lon,title){
-		var position = new google.maps.LatLng(lat,lon);
-		var marker = new google.maps.Marker({
-			position:position,
-			title:title
-		});
-		markers.push(newMarker);
-	};
+    $(".dropdown-toggle-hotels").on('click', 'li', function() {
+        $(this).parent().prev().text($(this).text());
+        var element = $(this);
 
-	function initialize_gmaps(inputMarkerLat,inputMarkerLon) {
-	 var markerLat = inputMarkerLat || 40.705786
-	 var markerLon = inputMarkerLon || -74.007672
-	  // initialize new google maps LatLng object
-	  var myLatlng = new google.maps.LatLng(40.705786,-74.007672); //need to query for this each time
-	 	console.log('LATLNG ',myLatlng)
-	  // set the map options hash
-	  var mapOptions = {
-	    center: myLatlng,
-	    zoom: 8,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP,
-	  };
-	 
-	  // get the maps div's HTML obj
-	  var map_canvas_obj = document.getElementById("map-canvas");
-	 
-	  // initialize a new Google Map with the options
-	  var map = new google.maps.Map(map_canvas_obj, mapOptions);
-	 
-	  // Add the marker to the map
-	  var marker = new google.maps.Marker({
-	    position: myLatlng,
-	    title:"Hello World!"
-	  });
+    });
 
-	  var marker2 = new google.maps.Marker({
-	  	position: new google.maps.LatLng(-.2,25),
-	  	title:'Africa'
-	  })
-	 
-	  // Add the marker to the map by calling setMap()
-	  // marker.setMap(map);
-	  // marker2.setMap(map)
-	  markers.forEach(function(element){
-	  	element.setMap(map)
-	  })
+    $("#add-hotel").on('click', function() {
+        console.log($(this).prev().first())
+        var element = $(this).prev().children().first().text();
+        $('#hotel-list').append('<p>' + element + '</p>');
+    });
 
-	}
-	 
-	
-	  initialize_gmaps();
+    $(".dropdown-toggle-restaurants").on('click', 'li', function() {
+        $(this).parent().prev().text($(this).text());
+        var element = null;
+        var element = $(this);
+
+
+    });
+
+    $("#add-restaurant").on('click', function() {
+        console.log($(this).prev().first())
+        var element = $(this).prev().children().first();
+        $('#restaurants-list').append('<p>' + element.text() + '</p>');
+
+        
+    });
+
+
+    $(".dropdown-toggle-things").on('click', 'li', function() {
+        $(this).parent().prev().text($(this).text());
+        var element = $(this);
+    });
+
+    $("#add-thing").on('click', function() {
+        console.log($(this).prev().first())
+        var element = $(this).prev().children().first().text();
+        $('#things-list').append('<p>' + element + '</p>');
+    });
+
+
+
+
+    ////Google Maps
+
+    var markers = [];
+
+
+    function newMarker(lat, lon, title) {
+        var position = new google.maps.LatLng(lat, lon);
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title
+        });
+        markers.push(newMarker);
+    };
+
+    function initialize_gmaps(inputMarkerLat, inputMarkerLon) {
+        var markerLat = inputMarkerLat || 40.705786
+        var markerLon = inputMarkerLon || -74.007672
+            // initialize new google maps LatLng object
+        var myLatlng = new google.maps.LatLng(40.705786, -74.007672); //need to query for this each time
+        console.log('LATLNG ', myLatlng)
+            // set the map options hash
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        };
+
+        // get the maps div's HTML obj
+        var map_canvas_obj = document.getElementById("map-canvas");
+
+        // initialize a new Google Map with the options
+        var map = new google.maps.Map(map_canvas_obj, mapOptions);
+
+        // Add the marker to the map
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            title: "Hello World!"
+        });
+
+        var marker2 = new google.maps.Marker({
+            position: new google.maps.LatLng(-.2, 25),
+            title: 'Africa'
+        })
+
+        // Add the marker to the map by calling setMap()
+        // marker.setMap(map);
+        // marker2.setMap(map)
+        markers.forEach(function(element) {
+            element.setMap(map)
+        })
+
+    }
+
+
+    initialize_gmaps();
 
 
 
